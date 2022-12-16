4d6a018cbb92e0e01f4c0859a2a99de8b7c750c7
DMcDonald57 <d.mcdonald579@gmail.com>
2022-12-15 21:03:04 -0700

    Log in page set up, not tested

--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -1,18 +1,28 @@
 <template>
     <div>
         <h1>Test Registration Page</h1>
-        <v-btn @click="apiCall">test api</v-btn>
+        <v-text-field
+        v-model="email"
+        />
+        <v-text-field
+        v-model="password"
+        />
+        <v-btn @click="login">Log In</v-btn>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
         name: "RegistrationPage",
 
         data() {
             return{
                 apiKey : process.env.VUE_APP_API_KEY,
-                apiUrl : process.env.VUE_APP_API_URL
+                apiUrl : process.env.VUE_APP_API_URL,
+                email : "",
+                password : ""
             }
         },
 methods: {
@@ -20,9 +30,26 @@ methods: {
         // Axios call
         console.log(this.apiKey);
         console.log(this.apiUrl);
-    }
+    },
+    login(){
+        axios.request({
+            method : "POST",
+            url : "https://foodierest.ml/api/client",
+            Headers : {
+                apiKey : process.env.VUE_APP_API_KEY
+            },
+            data : {
+                email : this.email,
+                password : this.password
+            }
+        }).then((response)=>{
+            console.log(response);
+        }).catch(() => {
+            console.log("Error");
+        })
+        }
+    },
 }
-    }
 </script>
 
 <style scoped>
