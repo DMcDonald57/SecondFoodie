3f39b914023df7f400dec94ef6adb792d54ffdb1
DMcDonald57 <d.mcdonald579@gmail.com>
2022-12-17 13:19:29 -0700

    started to add restaurant registration requirments

--- src/views/RegistrationPage.vue
+++ src/views/RegistrationPage.vue
@@ -1,6 +1,6 @@
 <template>
     <v-form>
-        <h1>Test Registration Page</h1>
+        <h1>Customer Registration</h1>
         <div>
             <v-row>
                 <v-col cols="12" sm="6" md="3">
@@ -21,6 +21,33 @@
             </v-row>
             <v-btn @click="registration">Customer Registration</v-btn>
         </div>
+        <div>
+            <h1>Restaurant Registration</h1>
+            <v-row>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="name" label="Restaurant Name" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="address" label="Address" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="bio" label="Slogan" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="City" label="City" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="email" label="Email" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="password" label="Password" solo dense></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-text-field v-model="phoneNum" label="Phone Number" solo dense></v-text-field>
+                </v-col>
+            </v-row>
+            <v-btn @click="registration">Restaurant Registration</v-btn>
+        </div>
     </v-form>
 </template>
 
@@ -39,14 +66,14 @@ import axios from 'axios';
                 lastName: "",
                 username: "",
                 password : "",
+                name : "",
+                address : "",
+                bio : "",
+                city : "",
+                phoneNum : ""
             }
         },
 methods: {
-    // apiCall()  {
-    //     // Axios call
-    //     console.log(this.apiKey);
-    //     console.log(this.apiUrl);
-    // },
     registration(){
         axios.request({
             method : "POST",
@@ -72,5 +99,12 @@ methods: {
 </script>
 
 <style scoped>
+
+h1 {
+
+    margin-bottom: 5vh;
+    margin-top: 5vh;
+
+}
 
 </style>
